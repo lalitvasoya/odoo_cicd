4cd8222d48710ed325302cf5e658d92ef224f8b5
lalitvasoya <lalit.vasoya@tntra.io>
2022-06-14 11:00:04 +0530

    [Add] new testcase.

--- tests/test_view.py
+++ tests/test_view.py
@@ -7,7 +7,12 @@ class TestFlaskRoute:
         assert response.json['status'] == "CICD"
         assert response.status == '200 OK'
 
-    def test_factorial_success(self, client):
+    def test_factorial_5_success(self, client):
         response = client.get('/fact', query_string={'no': 5})
         assert response.json['fact'] == 120
+        assert response.status == '200 OK'
+
+    def test_factorial_4_success(self, client):
+        response = client.get('/fact', query_string={'no': 4})
+        assert response.json['fact'] == 24
         assert response.status == '200 OK'
